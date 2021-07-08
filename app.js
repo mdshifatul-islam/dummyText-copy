b5c319d03771fe4b31be9104418128eba8e50c1a
mdshifatul-islam <86969168+mdshifatul-islam@users.noreply.github.com>
2021-07-08 21:52:07 +0600

    Upload the Dummy Text & Copy JS Project

---
+++ app.js
@@ -0,0 +1,52 @@
+const dummyText = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec eros molestie est finibus consectetur sed eu odio. Praesent at cursus sapien. Proin malesuada, augue eu imperdiet ultricies, mauris metus dignissim dui, a sagittis ipsum purus at ipsum. In congue ac velit et hendrerit. In hac habitasse platea dictumst. Vestibulum pellentesque leo a condimentum faucibus. Duis et odio vitae libero rutrum blandit. Vestibulum a mauris nec ligula malesuada commodo. Sed in mi nibh. Cras vehicula, arcu sed accumsan mattis, mi ante ultrices turpis, vel convallis lorem leo non ligula. Cras dui lacus, bibendum sit amet mi quis, eleifend imperdiet sem. Nunc rutrum leo at nibh imperdiet sodales. Suspendisse pellentesque magna sapien, ac hendrerit odio hendrerit eu. Vivamus venenatis non mauris id dignissim. Curabitur nunc orci, iaculis id tellus nec, bibendum ullamcorper ex.",
+
+"Nulla non suscipit magna. Maecenas vitae mi leo. Nullam commodo eleifend urna sit amet dictum. Praesent diam lacus, dictum et congue id, volutpat sodales risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus vel mauris at ex sollicitudin rutrum tempor a elit. Proin consectetur lobortis enim eget tristique. Maecenas in varius ante. Pellentesque pulvinar mauris a odio lobortis rhoncus.",
+
+"Cras semper non turpis quis finibus. Quisque tincidunt, elit nec imperdiet placerat, erat elit malesuada metus, ac fermentum velit lorem vel neque. Donec quis libero eu lorem faucibus congue sed ut quam. Etiam magna massa, auctor a molestie eu, laoreet sit amet turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec ut erat a velit rhoncus tempor quis ac mi. Morbi non libero tincidunt, mollis nibh quis, luctus nisi. Phasellus luctus libero sit amet erat pretium aliquet. Mauris arcu magna, porta nec est id, pretium placerat velit. Nam porta mollis gravida. Etiam aliquam quam et lacus congue, quis molestie est bibendum. Proin id lectus quam. Quisque ut rutrum lorem. Quisque non odio at ex scelerisque malesuada. In pretium auctor tortor, vel venenatis purus vulputate et.",
+
+"Donec malesuada, ex et interdum sodales, mi erat pellentesque velit, eu viverra leo massa eget ante. Nullam sit amet nulla non risus egestas condimentum. Aliquam pharetra libero lorem, id vulputate eros condimentum vel. Donec sapien nibh, lacinia ac est at, ultricies placerat quam. Integer vel enim tincidunt, feugiat est et, tempor risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam et orci nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus commodo tristique elit, tempor pretium sem sagittis eget. Nullam sagittis est nulla, et pharetra arcu auctor quis. In nibh felis, congue quis consequat at, aliquam et massa.",
+
+"Quisque lacus purus, finibus in eros ac, feugiat fermentum elit. Sed metus augue, rutrum ut fermentum a, placerat nec leo. Sed ex lacus, volutpat eget laoreet non, molestie a augue. Duis in elementum erat. Cras tortor diam, dapibus vel nunc ut, pretium blandit est. Cras accumsan nunc blandit urna egestas lacinia. In tempor, ex in convallis pulvinar, lorem ante faucibus metus, et laoreet lacus mauris sed leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In convallis ante pellentesque porta ultricies. Mauris semper urna mi, ac vestibulum augue dapibus a. Quisque est dui, ullamcorper ut massa id, varius consectetur elit. Nam condimentum odio et ante lobortis, id hendrerit orci luctus. Sed venenatis et tortor eu viverra. Cras dictum mi ac viverra dictum.",
+
+"Aliquam vel tortor felis. Curabitur vel vestibulum est. Nunc accumsan, nibh sit amet volutpat lacinia, nisi odio laoreet quam, efficitur dapibus tortor velit non lorem. Ut et ipsum augue. Donec lorem mi, imperdiet fermentum sagittis eget, accumsan eu nunc. In eu cursus lectus. In molestie dui libero. Vivamus eget dui sem. Sed dapibus tortor lacus. Curabitur vehicula enim et ornare consequat. Sed placerat ligula mauris, at molestie nisl iaculis egestas. Aliquam erat volutpat. Morbi sodales tellus eu leo tincidunt cursus. Cras viverra sit amet orci egestas venenatis. Sed feugiat bibendum erat vitae viverra. In eu augue ac enim faucibus tempor.",
+
+"Pellentesque vitae rutrum nibh. Mauris accumsan justo dignissim ante rutrum luctus. Integer nulla diam, tristique in ex ut, volutpat ullamcorper ante. In congue est id dapibus dignissim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas condimentum a arcu nec elementum. Maecenas diam eros, dignissim consequat viverra quis, mattis eu diam. Aliquam congue dictum lectus, non pulvinar erat maximus et. Phasellus non turpis at nunc gravida condimentum nec quis turpis. Etiam vel justo diam. Nunc nisi tortor, tincidunt ut felis sed, pharetra tempus massa. Nam nulla nisl, iaculis ac lobortis non, sodales et nibh. Sed laoreet lacus leo, ut accumsan ex ultrices at.",
+
+"Suspendisse eleifend tellus turpis, at accumsan risus lobortis vitae. Donec sed nibh ut eros tincidunt ultricies. Vivamus sagittis facilisis dui, vitae auctor mauris luctus at. Donec faucibus ex justo, non maximus ipsum lacinia a. Proin vulputate rutrum ipsum, sit amet dapibus massa egestas quis. Mauris sed turpis mattis nunc pharetra consequat ac in felis. Proin at tortor dolor. Aenean ut nulla magna. Aliquam erat volutpat. Integer quis velit feugiat, faucibus nisl sed, volutpat lacus. Sed id ante sodales, dictum ipsum eu, maximus felis. Integer suscipit nec lorem commodo interdum. Integer nunc odio, pretium at fermentum non, ultricies id purus. Pellentesque eget lobortis magna, vitae ullamcorper lectus.",
+
+"Donec faucibus ante consectetur fringilla venenatis. Praesent sagittis, sapien vitae sagittis mattis, lectus elit sagittis justo, in suscipit est orci quis mi. Maecenas pretium eleifend sapien, vitae pharetra sapien molestie id. Vivamus lacinia in diam ac imperdiet. Morbi risus orci, dictum eget lacus ac, feugiat aliquet sem. Nam ac aliquam libero. Mauris feugiat mollis justo, in efficitur urna euismod in. Morbi semper urna eros, fermentum venenatis libero sagittis et. Ut a convallis lorem, sit amet pulvinar orci. Donec placerat quam quam, rhoncus pharetra enim mattis a.",
+
+"Nullam dictum est quis tellus fermentum malesuada. Vivamus lacinia rutrum augue porta rhoncus. Aenean accumsan eu ipsum ac vulputate. Fusce ut lacus sollicitudin, hendrerit ante eu, rhoncus dolor. Morbi efficitur rutrum auctor. Donec sed pharetra odio, sit amet volutpat velit. Nam dapibus fermentum lacus sit amet egestas. Duis eu egestas libero. Morbi tincidunt, lectus sit amet eleifend sollicitudin, mi purus tempor orci, vel mattis magna massa ut ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent et elit dignissim, fringilla ex eget, laoreet justo. Nunc semper viverra consequat. Fusce in ipsum non est iaculis cursus. In consequat accumsan tortor at accumsan. Curabitur et imperdiet sem, eu varius sapien."]
+
+const form = document.querySelector('#form');
+const numberBox = document.querySelector('#number');
+const paraBox = document.querySelector("#paraBox")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let num = parseFloat(numberBox.value);
+    let arrayLen = dummyText.length;
+
+    if(isNaN(num) || num > 10 || num === 0 || num < 0){
+        let random = Math.floor(Math.random() * arrayLen);
+        paraBox.innerHTML = `<p>${dummyText[random]}</p>`;
+    }else{
+        let arraySlice = dummyText.slice(0, num);
+        let newArray = arraySlice.map(function(allPara){
+            return `<p>${allPara}</p>`
+        }).join();
+        paraBox.innerHTML = newArray;
+    }
+});
+
+//Start the Copy To Clipboard
+
+const text = document.querySelector("#text");
+const button = document.querySelector("#copy_btn");
+
+button.addEventListener('click', () => {
+    text.select();
+    document.execCommand('copy');
+    alert("Copied!!!")
+})
